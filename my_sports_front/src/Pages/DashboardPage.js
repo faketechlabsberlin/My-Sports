cd8f955d414ccd645438855ca1f3f944deb11bad
Cátia <YOUR GITHUB EMAIL>
2021-01-27 08:16:56 +0000

    Dashboard / Find Events page

--- my_sports_front/src/Pages/DashboardPage.js
+++ my_sports_front/src/Pages/DashboardPage.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { useEffect } from "react";
-import { connect } from "react-redux";
+import React, { useEffect, useState } from 'react'
+import { connect, useSelector } from "react-redux";
 import { logout } from "../actions/session";
 import { getEvents } from '../actions/event';
 import { resetFilters } from '../actions/filter';
@@ -8,10 +7,9 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { Card } from 'react-bootstrap';
 import Header from '../components/Header';
-import footballicon from '../images/Button_styles/football.png';
 import { clearErrors } from "../actions/error";
 import { clearSuccess } from "../actions/success";
-
+import StarRatings from 'react-star-ratings';
 
 const mapStateToProps = ({ session, event }) => ({
   session,
@@ -26,7 +24,6 @@ const mapDispatchToProps = dispatch => ({
   clearSuccess: () => dispatch(clearSuccess())
 });
 
-
 const DashboardPage = ({ logout, session, event, getEvents, resetFilters, clearErrors, clearSuccess }) => {
 
   useEffect(async () => {
@@ -34,51 +31,67 @@ const DashboardPage = ({ logout, session, event, getEvents, resetFilters, clearE
     resetFilters();
     clearErrors();
     clearSuccess();
-  }, [])
+    const div = document.getElementById('border-bottom-color')
+    div.classList.add(`bottom-border-color-${event.sport}`)
+  },
+    [])
+
   return (
     <div>
-      <Header />
+      <div id="header-div">
+        <Header />
+      </div>
+      <div id="border-bottom" />
       <div id="welcome">
         <p>Welcome {session.username}! </p>
         <p>You can now join a match, create an event or tell us your preferencies and have a look at matches we found for you.</p>
         <p><Link id="pref-link" to={"/profile/" + session.userId}>Edit preferencies</Link></p>
       </div>
       <div id="dashcontent">
-        <p>View former Events here: * Short list of recent events *</p>
         <p>Your upcoming matches:</p>
-
         <div>
 
           {event.filter(e => e.teammates.some(teammate => teammate._id === session.userId)).map((myEvents) => {
             return <div>
-              <div id="card">
-                <Card>
-                  <Link to={"/event/" + myEvents._id}>
-                    <Card.Header id="cardHeader">{myEvents.title}</Card.Header>
-                  </Link>
-                  <Card.Body>
-                    <blockquote className="blockquote mb-0" id="eventInfo">
-                      <div id="dateLocation">
-                        <p>
-                          {' '}
-                          {moment(myEvents.date).format("ddd, MMMM Do, ha")}
-                        </p>
-                        <p>
-                          {myEvents.location}
-                        </p>
-
-                      </div>
-                      <p className="sport-icon" id="icone">
-                      </p>
-
-                    </blockquote>
-                    <div id="cardFooter">
-                      <p>Players</p>
-                      <p>level</p>
+              <Card className="card-event">
+                <Link to={"/event/" + myEvents._id}>
+                  <Card.Header id="cardHeader">{myEvents.title}</Card.Header>
+                </Link>
+                <Card.Body>
+                  <blockquote className="blockquote mb-0" id="eventInfo">
+                    <div className="dateLocation" id="border-bottom-color">
+                      <p>{moment(myEvents.date).format("ddd, MMMM Do, ha")}</p>
+                      <p>{myEvents.location}</p>
                     </div>
-                  </Card.Body>
-                </Card>
-              </div>
+                    <img className="sport-icon" id="icone" src={`../images/sport-images/${myEvents.sport}.png`} />
+                  </blockquote>
+                  <div id="cardFooter">
+                    <p className="card-text text-muted player-count-text">Players: <span className="error-text">{myEvents.teammates.length}/{myEvents.size}</span></p>
+                    <p className="card-text text-muted player-count-text">Skill Level:
+                      <StarRatings
+                        rating={myEvents.SkillLevel}
+                        starRatedColor="#E9B467"
+                        numberOfStars={5}
+                        starDimension="1.2em"
+                        starSpacing="0.7px"
+                      />
+                    </p>
+                  </div>
+                </Card.Body>
+                <div className="accordion" id="accordionExample">
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                      </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                      <div className="accordion-body">
+                        <button className="edit-event-button"><Link className="edit-event-link" to={"/event/" + myEvents._id}>Event Details</Link></button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Card>
             </div>
           })}
         </div>
--- my_sports_front/src/components/Side_Bar.js
+++ my_sports_front/src/components/Side_Bar.js
@@ -1,41 +1,42 @@
-import React from "react";
+import React from 'react'
 import { slide as Menu } from "react-burger-menu";
+import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
 
-export default props => {
+const mapStateToProps = ({ session, event }) => ({
+    session,
+    event
+});
+
+const Side_Bar = ({ props, event, session }) => {
+
     return (
         <div id="Menu">
             <Menu {...props}>
-
                 <div className="top-menu">
-                    <p>Edit profile</p>
-                    <p>Username</p>
-                    <p>Username.com</p>
+                    <div>
+                        {event.teammates && event.teammates.map((user, index) => {
+                            return <div key={user._id} className={"col teammate-icon teammate-icon-color-" + index}><h6>{user.username[0].toUpperCase()}</h6></div>
+                        })}
+                    </div>
+                    <p><Link to="/profile" id="edit-profile">Edit profile</Link></p>
+                    <p id="username">{session.username}</p>
                 </div>
-
-
                 <div id="search-div">
                     <input type="text" className="search-menu" placeholder=" Search for event name" />
                 </div>
                 <div className="content-menu">
-                    <Link to="/create-event" className="menu-item">
-                        Create Event
-      </Link>
-
-                    <Link to="/find-event" className="menu-item">
-                        Find events
-      </Link>
-
-                    <Link to="/" className="menu-item">
-                        Upcoming
-      </Link>
-
-                    <Link to="/" className="menu-item">
-                        Played
-      </Link>
+                    <Link to="/create-event" className="menu-item">Create Event</Link>
+                    <Link to="/find-event" className="menu-item">Find events</Link>
+                    <Link to="/" className="menu-item">Upcoming</Link>
+                    <Link to="/" className="menu-item">Played</Link>
                 </div>
             </Menu>
 
         </div >
+
     );
 };
+
+export default connect(
+    mapStateToProps)(Side_Bar);
